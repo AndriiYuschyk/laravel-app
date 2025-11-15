d854e77765f36bbbdf1b0fb10aa5bd80c6a0726f
Andrii Yushchyk <andriiyuschyk@gmail.com>
2025-11-15 16:40:06 +0200

    Created CompanyVersion model and migration

---
+++ app/Models/CompanyVersion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CompanyVersion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'version',
+        'name',
+        'address',
+    ];
+
+    public function companies(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
